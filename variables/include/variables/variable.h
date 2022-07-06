37c3fab10aa68a6210ae641a3ffbdab0472a1dbf
SmartPolarBear <clevercoolbear@outlook.com>
2022-07-06 23:05:38 +0800

    chore(project): better project structure

--- variables/include/variables/variable.h
+++ variables/include/variables/variable.h
@@ -6,11 +6,22 @@
 
 #include <computational_graph/node.h>
 
+#include <xtensor/xarray.hpp>
+#include <Eigen/Core>
+
+
 namespace cppag::variables
 {
 class variable
 	: public computational_graph::node
 {
+public:
+	using value_type= double;
+private:
+	bool is_constant_;
+	std::vector<std::shared_ptr<computational_graph::node>> children_;
+	std::weak_ptr<computational_graph::node> parent_;
+
 
 };
 }
