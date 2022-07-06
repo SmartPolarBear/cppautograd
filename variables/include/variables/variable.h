464ed533b82f5147d15c3097f382a6a4ce836a1b
SmartPolarBear <clevercoolbear@outlook.com>
2022-07-06 14:37:50 +0800

    chore(project): add 3rd-party libraries

---
+++ variables/include/variables/variable.h
@@ -0,0 +1,16 @@
+//
+// Created by bear on 7/5/2022.
+//
+
+#pragma once
+
+#include <computational_graph/node.h>
+
+namespace cppag::variables
+{
+class variable
+	: public computational_graph::node
+{
+
+};
+}
---
+++ variables/variable.cpp
@@ -0,0 +1,4 @@
+//
+// Created by bear on 7/6/2022.
+//
+#include <variables/variable.h>
